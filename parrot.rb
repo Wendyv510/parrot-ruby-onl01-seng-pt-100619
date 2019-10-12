565d7f1b98d72da8d8e26204eb832e4e99c296d1
Wendy Harris <wendyv510@hotmail.com>
2019-10-12 14:28:13 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
 def parrot (says="Squawk")
   puts "#{says}"
 end 
 
-parrot("Polly want a cracker")
+
+says="Polly want a cracker"
+parrot("says")
 
